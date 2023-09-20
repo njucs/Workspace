df77bf61050005991a398bcc7b1037ba417fd30c
Liang <njucs2001@hotmail.com>
2023-09-20 15:07:08 +0800

    add LLM-CMB

---
+++ LLM-CMB/cmb.py
@@ -0,0 +1,10 @@
+
+# CMB, A Comprehensive Medical Benchmark in Chinese
+
+from datasets import load_dataset
+# CMB-Exam datasets （multiple-choice and multiple-answer questions）
+# exam_datasets = load_dataset('FreedomIntelligence/CMB','exam')
+# CMB-Clin datasets
+# clin_datasets = load_dataset('FreedomIntelligence/CMB','clin')
+
+
