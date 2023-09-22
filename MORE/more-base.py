0594a00a769d9d7a7833d7fe5b8d722e92fa32d9
Liang <njucs2001@hotmail.com>
2023-09-22 16:19:03 +0800

    update code

---
+++ MORE/more-base.py
@@ -0,0 +1,25 @@
+'''
+{
+"none":0,
+"/per/per/opponent":1,      //竞争对手
+"/per/per/partner":2,        //合作伙伴
+"/per/per/relatives":3,      //亲属，包括父母、夫妻等
+"/per/per/alternate_names":4,     //同一个人的不同名称，例如某人扮演了某个角色，某个人的称号
+"/per/org/opposed_to":5,     //人与组织之间的竞争关系
+"/per/org/leader_of":6,      //领导
+"/per/org/member_of":7,         //成员
+"/per/loc/place_of_birth":8,     //某人的出生地
+"/per/loc/place_of_residence":9,     //某人到达/居住在某地
+"/per/loc/place_of_governance":10,     //某人的政治管辖范围
+"/per/misc/present_in":11,     //参与到
+"/per/misc/awarded":12,         //被授予奖项，称号
+"/per/misc/party":13,         //党派，政治派别
+"/per/misc/nationality":14,   //国籍
+"/per/misc/president":15,     //总统
+"/org/org/alternate_names": 16,     //同一个组织的不同名称
+"/org/org/subsidiary":17,     //组织之间的上下级关系
+"/org/loc/locate_at":18,         //坐落在
+"/org/misc/present_in": 19,         //参与到
+"/loc/loc/contain":20          //地点之间的包含关系
+}
+'''
